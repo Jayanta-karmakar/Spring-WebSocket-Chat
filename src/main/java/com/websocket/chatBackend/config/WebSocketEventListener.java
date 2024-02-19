69225fdd9bf43f4e035f9d39fd64dc8c17779b7f
Jayanta Karmakar <jayantakarmakar998@gmail.com>
2024-02-19 23:46:38 +0530

    restructure

--- src/main/java/com/websocket/chatBackend/config/WebSocketEventListener.java
+++ src/main/java/com/websocket/chatBackend/config/WebSocketEventListener.java
@@ -4,11 +4,14 @@ import com.websocket.chatBackend.model.ChatMessage;
 import com.websocket.chatBackend.model.MessageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import java.util.Objects;
 
 /**
  * @author : jayantakarmakar
@@ -23,9 +26,10 @@ public class WebSocketEventListener {
     private final SimpMessageSendingOperations messagingTemplate;
 
     @EventListener
-    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+    public void handleWebSocketDisconnectListener(@NotNull SessionDisconnectEvent event) {
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
-        String username = headerAccessor.getSessionAttributes().get("username").toString();
+        String username = Objects.requireNonNull(headerAccessor.getSessionAttributes())
+                .get("username").toString();
         if (username != null) {
             log.info("User Disconnected: {}", username);
             var chatMessage = ChatMessage.builder()
